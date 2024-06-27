8174266eb1dcd1267f3514a30d9cde5db0f17981
ibrahimabdullahibare <ibrahimabdullahibare@gail.com>
2024-06-27 09:05:50 -0900

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// Write your code here!
+
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+    mainElement.remove();
+}
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+
+newHeader.textContent = `${yourName} is the champion`;
+
+document.body.appendChild(newHeader);
